e24552f22afec1d3f64b895c420c8b01980e8c85
tomual <tomual@users.noreply.github.com>
2017-07-16 21:01:29 -0500

    Forum and thread basic view

--- forums/urls.py
+++ forums/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    # /thread/5/
+    url(r'^(?P<thread_id>[0-9]+)/$', views.view, name='detail'),
+    # /thread/5/respond/
+    url(r'^(?P<thread_id>[0-9]+)/respond/$', views.respond, name='respond'),
+
 ]
--- forums/views.py
+++ forums/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import Thread
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_thread_list = Thread.objects.order_by('-thread_date')[:5]
+    context = {
+        'latest_thread_list': latest_thread_list,
+    }
+    return render(request, 'forums/index.html', context)
+
+def view(request, thread_id):
+    thread = get_object_or_404(Thread, pk=thread_id)
+    context = {
+        'thread': thread,
+    }
+    return render(request, 'forums/view.html', context)
+
+def respond(request, thread_id):
+    return HttpResponse("Replying to thread %s" % thread_id)
