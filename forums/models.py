c6f1414e3963260f7d20b46583dd1161c84aa8e6
tomual <tomual@users.noreply.github.com>
2017-07-16 20:00:39 -0500

    Uploaded app instead of project

---
+++ forums/models.py
@@ -0,0 +1,24 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+class Thread(models.Model):
+    thread_title = models.CharField(max_length=200)
+    thread_content = models.CharField(max_length=200)
+    thread_date = models.DateTimeField('date posted')
+
+    def __str__(self):
+    	return self.thread_title
+
+    def was_posted_recently(self):
+    	return self.thread_date >= timezone.now() - datetime.timedelta(days = 1)
+
+
+class Response(models.Model):
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    response_content = models.CharField(max_length=200)
+    reply_date = models.DateTimeField('date posted')
+
+    def __str__(self):
+    	return self.response_content[:10]
