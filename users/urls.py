0ec29366a58b529e744af94fdac4139ad45b4f6d
tomual <tomual@users.noreply.github.com>
2017-08-03 22:20:48 -0500

    User registration

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login$', views.login_view, name='login'),
+    url(r'^logout$', views.logout_view, name='logout'),
+    url(r'^register$', views.register_view, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import Permission, User
 from django.contrib import messages
-
-
 
 def index(request):
     return render(request, 'users/index.html')
@@ -26,3 +25,18 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('/')
+
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        user = User.objects.create_user(username, email, password)
+        if user is not None:
+            login(request, user)
+            messages.error(request, 'Yay.')
+        else:
+            messages.error(request, 'Invalid login.')
+        return render(request, 'users/register.html')
+    else:
+        return render(request, 'users/register.html')
