e20b0b05d017ef578a999f6ebbddc0a3f91de2a2
tomual <tomual@users.noreply.github.com>
2017-08-15 21:26:16 -0500

    Add Profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):  
+    user = models.OneToOneField(User)
+    location = models.CharField(max_length=140, blank=True)
+    description = models.CharField(max_length=255, blank=True)
+    icon = models.ImageField(upload_to='icons', blank=True)
+
+    def __unicode__(self):
+        return u'Profile of user: %s' % self.user.username
